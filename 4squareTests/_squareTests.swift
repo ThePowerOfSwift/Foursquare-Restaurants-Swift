8eab33cd005a0f67813a7a5ae457f011b19f2dee
Mihail Salari <SEN-6694784SEN-6694784info@mihailsalari.com>
2016-04-11 02:51:25 +0300

    Initial Commit

---
+++ 4squareTests/_squareTests.swift
@@ -0,0 +1,36 @@
+//
+//  _squareTests.swift
+//  4squareTests
+//
+//  Created by Mihail Șalari on 11.04.2016.
+//  Copyright © 2016 PlatinumCode. All rights reserved.
+//
+
+import XCTest
+@testable import _square
+
+class _squareTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
